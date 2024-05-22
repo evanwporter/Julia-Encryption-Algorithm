aee6619eb89e3111d1281f71e47f4e85576d209e
Evan <115374841+evanwporter@users.noreply.github.com>
2024-05-22 09:25:29 -0700

    Delete Julia directory

--- Julia/jea/__init__.py
+++
@@ -1,3 +0,0 @@
-from .encrypt import encrypt
-from .decrypt import decrypt
-from .julia import julia_set
--- Julia/jea/encrypt.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def encrypt(img, julia):
-    height, width, _ = img.shape
-    return (
-        (
-            img.flatten() + (julia % 256).astype(np.uint8)
-        ) % 256
-    )[np.argsort(julia)].reshape((height, width, 3))
